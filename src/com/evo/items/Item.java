ca120ed9ef3fcee9e9d9401aaa6db22ae5b1a42c
jamesgov <blindmice1o3@gmail.com>
2019-08-24 15:41:30 -0700

    feat. Item (objects dropped after an entity's die() is call, e.g. meat) class tick() to check if the player's collision bounds intersects() with its own bounds, if so (AS WELL AS KeyEvent.VK_SPACE [button that triggers eating state] being just pressed) set this item's count to PICKED_UP (ItemManager.tick() checks count to see which items in its ArrayList<Item> should be removed). NEXT: give player a member field to store experience points, then in Item.tick() add the logic to increment the player's experience points AND increment the player's health by some specified amount.

--- src/com/evo/items/Item.java
+++ src/com/evo/items/Item.java
@@ -1,27 +1,31 @@
 package com.evo.items;
 
 import com.evo.Handler;
+import com.evo.entities.moveable.fish.Fish;
 import com.evo.gfx.Assets;
+import com.evo.states.GameStageState;
+import com.evo.states.StateManager;
 
 import java.awt.*;
+import java.awt.event.KeyEvent;
 import java.awt.image.BufferedImage;
 
 public class Item {
 
     //HANDLER
-
     public static Item[] items = new Item[256];
     public static Item meatItem = new Item(Assets.meat, "Meat", 0);
 
     //CLASS
-
-    public static final int ITEMWIDTH = 32, ITEMHEIGHT = 32, PICKED_UP = -1;
+    public static final int ITEMWIDTH = 16, ITEMHEIGHT = 16, PICKED_UP = -1;
 
     protected Handler handler;
 
     protected BufferedImage texture;
     protected String name;
     protected final int id;
+
+    protected Rectangle bounds;
 
     protected int x, y, count;
 
@@ -31,11 +35,22 @@ public class Item {
         this.id = id;
         count = 1;
 
+        //x and y are defaulting to 0 at the moment, they get set/initialize when setPosition() is called.
+        bounds = new Rectangle(x, y, ITEMWIDTH, ITEMHEIGHT);
+
         items[id] = this;
     } // **** end Item(BufferedImage, String, int) constructor ****
 
     public void tick() {
+        Fish player = ((GameStageState)handler.getStateManager().getState(StateManager.State.GAME_STAGE)).getCurrentGameStage().getPlayer();
 
+        //add && condition where KeyManager.keyJustPressed( button-that-triggers-eating-state ).
+        //player stepped on this item and that means it should get picked up.
+        if ( (player.getCollisionBounds(0, 0).intersects(bounds)) &&
+                (handler.getKeyManager().keyJustPressed(KeyEvent.VK_SPACE)) ) {
+            //TODO: give player health/experience.
+            count = PICKED_UP;
+        }
     }
 
     /**
@@ -66,6 +81,8 @@ public class Item {
     public void setPosition(int x, int y) {
         this.x = x;
         this.y = y;
+        bounds.x = x;
+        bounds.y = y;
     }
 
     // GETTERS AND SETTERS
